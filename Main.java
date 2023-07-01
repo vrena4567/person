12b7090ebbacfcffa3b27e551418b67a6a088079
vrena4567 <v.rena4567@gmail.com>
2023-07-01 09:38:26 +0200

    correct rdy

--- Main.java
+++ Main.java
@@ -7,21 +7,12 @@ public class Main {
         List<Person> personList = new ArrayList<>();
         List<String> listOfPerson = FileReader.fileReader("person.txt");
         System.out.println(listOfPerson);
-        String name = "dfa";
-        int age = 2;
-        String gender = "fda";
 
-        for (int i = 0; i < listOfPerson.size(); i++) {
-            for (var actualElement : listOfPerson.get(i).split(",")) {
-                if (actualElement.length() > 5) {
-                    name = actualElement;
-                } else if (actualElement.equals("nő") || actualElement.equals("férfi")) {
-                    gender = actualElement;
-                } else if (actualElement.length() < 3) {
-                    age = Integer.parseInt(actualElement);
-
-                }
-            }
+        for (var line : listOfPerson) {
+            String[] lineAsArray = line.split(",");
+            String name = lineAsArray[0];
+            int age = Integer.parseInt(lineAsArray[1]);
+            Gender gender = lineAsArray[2].equals("nő") ? Gender.FEMALE : Gender.MALE;
             Person person = new Person(name, age, gender);
             personList.add(person);
         }
--- Person.java
+++ Person.java
@@ -1,9 +1,9 @@
 public class Person {
     String name;
     int age;
-    String gender;
+    Gender gender;
 
-    public Person(String name, int age, String gender) {
+    public Person(String name, int age, Gender gender) {
         this.name = name;
         this.age = age;
         this.gender = gender;
